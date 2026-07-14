a9d8a39b249cf55dc59691c3c1dbc0b7f8f53bc4
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-14 11:50:49 +0700

    fix build

---
+++ SynaptiX_FDK/app/app.c
@@ -0,0 +1,12 @@
+#include "app.h"
+#include "logger.h"
+#include "sx_delay.h"
+
+static const char *TAG = "APP";
+
+void app_init(void){
+    log_info(TAG, "APP initializing ....");
+}
+void app_process(void){
+    
+}

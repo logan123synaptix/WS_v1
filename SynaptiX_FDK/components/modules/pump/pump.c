d3239f319ffd804741a24dc33857d6c2c4aaabf3
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-14 14:08:05 +0700

    pump on fix

--- SynaptiX_FDK/components/modules/pump/pump.c
+++ SynaptiX_FDK/components/modules/pump/pump.c
@@ -4,7 +4,7 @@ void pump_init(sx_gpio_t *gpio, sx_gpio_ops_t *ops, void *pDriver){
     sx_gpio_init(gpio, ops, pDriver);
 }
 
-void void pump_on(sx_gpio_t *gpio){
+void pump_on(sx_gpio_t *gpio){
     sx_gpio_write(gpio, PUMP_TURN_ON);
 }
 
